6938682ee09c1b2059b77aec06b653644a6e967f
federico-creator <fbornico@udesa.edu.ar>
2021-09-02 10:27:57 -0300

    comenzando

---
+++ proyecto/src/Components/Footer/Footer.js
@@ -0,0 +1,18 @@
+import React  from "react"
+import "./Footer.css"
+
+//por ahora es sin estado, seguro despues hay que cambiarlo
+function Footer() {
+    return (
+        <footer>
+        <ul class="team">
+            <li>Nombre integrante 1</li>
+            <li>Nombre integrante 2</li>
+            <li>Nombre integrante 3</li>
+        </ul>
+    </footer>
+   
+    );
+  }
+  
+  export default Footer;
---
+++ proyecto/src/Components/Tarjetas/Tarjetas.js
@@ -0,0 +1,27 @@
+import React, {Component} from "react"
+
+
+class RickMorty extends Component{
+
+    constructor(){
+        super()
+        this.state={
+            
+        }
+    }
+
+
+    render(){
+        
+        return(
+            <>
+
+                <h1>parte de tarjetas</h1>
+
+            </>
+        )
+    }
+}
+
+
+export default RickMorty
