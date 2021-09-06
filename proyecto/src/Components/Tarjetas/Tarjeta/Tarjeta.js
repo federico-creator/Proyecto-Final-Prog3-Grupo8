ca192b7cf9e062aef1f774acd5ba2e6187f6f317
isaacyoon <iyoon@udesa.edu.ar>
2021-09-06 11:38:03 -0300

    punto 2

--- proyecto/src/Components/Tarjetas/Tarjeta/Tarjeta.js
+++ proyecto/src/Components/Tarjetas/Tarjeta/Tarjeta.js
@@ -3,9 +3,18 @@ import "./Tarjeta.css"
 
 //por ahora es sin estado, seguro despues hay que cambiarlo
 function Tarjeta(props) {
+    console.log(props)
+    const{
+        title, overview, poster_path, vote_average, adult
+        } = props.caracteristicas
     return (
-        <p>Hola mundo</p>
-   
+    <div>
+    <img className='imagen' src={`https://image.tmdb.org/t/p/original/${poster_path}`} />
+    <h1>{title}</h1>
+    <h3>{overview}</h3>
+    <h3>{vote_average}</h3>
+    <h3>{adult}</h3>
+   </div>
     );
   }
   
